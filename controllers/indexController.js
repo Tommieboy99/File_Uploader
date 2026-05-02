d2d949dd531411614d7e82bbc366175c32be5185
Tommieboy99 <tomkoek99@hotmail.com>
2026-05-02 13:11:28 +0200

    Added createFolderErrors session data, to display the errors/give user feedback when creating fails

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,6 +3,9 @@ import { prisma } from "../lib/prisma.js"
 
 export const getIndexPage = async (req, res) => {
     if (req.user) {
+        
+        const createFolderErrors = req.session.createFolderErrors || null
+        delete req.session.createFolderErrors
 
         const folders =  await prisma.folder.findMany({
             where: { userId: req.user.id, isRoot: false }
@@ -17,7 +20,7 @@ export const getIndexPage = async (req, res) => {
             }
         })
 
-        res.render('dashboard', { user: req.user, folders, rootFiles })
+        res.render('dashboard', { user: req.user, folders, rootFiles, createFolderErrors })
     } else {
         res.render('homepage', { user: null })
     }
