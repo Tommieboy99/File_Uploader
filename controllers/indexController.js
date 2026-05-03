3a8bcee98c3fbf4256b7588692eb268eb88eae1b
Tommieboy99 <tomkoek99@hotmail.com>
2026-05-03 22:48:04 +0200

    added the fileUploadErrors to display in the view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,9 +3,10 @@ import { prisma } from "../lib/prisma.js"
 
 export const getIndexPage = async (req, res) => {
     if (req.user) {
-        
         const createFolderErrors = req.session.createFolderErrors || null
+        const fileUploadErrors = req.session.fileUploadErrors || null
         delete req.session.createFolderErrors
+        delete req.session.fileUploadErrors
 
         const folders =  await prisma.folder.findMany({
             where: { userId: req.user.id, isRoot: false }
@@ -20,7 +21,7 @@ export const getIndexPage = async (req, res) => {
             }
         })
 
-        res.render('dashboard', { user: req.user, folders, rootFiles, createFolderErrors })
+        res.render('dashboard', { user: req.user, folders, rootFiles, createFolderErrors, fileUploadErrors })
     } else {
         res.render('homepage', { user: null })
     }
