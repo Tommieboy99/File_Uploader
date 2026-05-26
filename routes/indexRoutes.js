96c66835bce62ca5cedf35a09dc2e3823ab7c44e
Tommieboy99 <tomkoek99@hotmail.com>
2026-05-26 21:32:34 +0200

    index routes

---
+++ routes/indexRoutes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { getIndexPage } from "../controllers/indexController.js";
+const indexRouter = Router();
+
+indexRouter.get('/', getIndexPage);
+
+export { indexRouter };
